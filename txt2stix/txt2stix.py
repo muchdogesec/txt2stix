452dc51b6e2ea95978fd4671986d795f026add3b
David G <himynamesdave@users.noreply.github.com>
2024-08-14 09:50:01 +0100

    fixing required vars

--- txt2stix/txt2stix.py
+++ txt2stix/txt2stix.py
@@ -133,7 +133,7 @@ def parse_args():
     return args
 
 REQUIRED_ENV_VARIABLES = [
-    "INPUT_CHARACTER_LIMIT",
+    "INPUT_TOKEN_LIMIT",
     "ARANGODB_HOST_URL",
     "ARANGODB_USERNAME",
     "ARANGODB_PASSWORD",
@@ -144,8 +144,8 @@ def load_env(input_length):
     for env in REQUIRED_ENV_VARIABLES:
         if not os.getenv(env):
             raise FatalException(f"env variable `{env}` required")
-    # if input_length > int(os.environ["INPUT_CHARACTER_LIMIT"]):
-    #     raise FatalException(f"input_file length ({input_length}) exceeds character limit ({os.environ['INPUT_CHARACTER_LIMIT']})")
+    # if input_length > int(os.environ["INPUT_TOKEN_LIMIT"]):
+    #     raise FatalException(f"input_file length ({input_length}) exceeds character limit ({os.environ['INPUT_TOKEN_LIMIT']})")
 
 def extract_all(bundler: txt2stixBundler, extractors_map, aliased_input, ai_extractor: BaseAIExtractor=None):
     all_extracts = dict()
