814226bb1212fd19c68eaf65c3c4e4f44afd20da
Fadl <chaos@efqr.dev>
2024-11-19 15:50:57 +0100

    initial impl ignore_image_refs (#68) and ignore_link_refs (#70)

--- txt2stix/utils.py
+++ txt2stix/utils.py
@@ -1,14 +1,35 @@
 import pkgutil
 import re
 from pathlib import Path
+from typing import Dict
+
+import mistune
+from mistune.renderers.markdown import MarkdownRenderer
+from mistune.util import unescape
+class ImageLinkRemover(MarkdownRenderer):
+    def __init__(self, remove_links: bool=False, remove_images: bool=False):
+        self.remove_links = remove_links
+        self.remove_images = remove_images
+        super().__init__()
+
+    def image(self, token: dict[str, dict], state: mistune.BlockState) -> str:
+        if self.remove_images:
+            return ''
+        return super().image(token, state)
+
+    def link(self, token: dict[str, dict], state: mistune.BlockState) -> str:
+        if self.remove_links and token.get('type') != 'image':
+            return self.render_children(token, state)
+        return super().link(token, state)
+    
+    def codespan(self, token: dict[str, dict], state: mistune.BlockState) -> str:
+        token['raw'] = unescape(token['raw'])
+        return super().codespan(token, state)
 
 
-def remove_data_images(input_text):
-    match_re = re.compile(r"(!\[([^\]]*)\]\((data:image/\w+;\w+),([^\^)]+)\))")
-    def subber(match, *args):
-        return "![{}]({},blank...)".format(match.group(2), match.group(3))
-    return match_re.sub(subber, input_text)
-
+def remove_links(input_text: str, remove_images: bool, remove_anchors: bool):
+    modify_links = mistune.create_markdown(escape=False, renderer=ImageLinkRemover(remove_links=remove_anchors, remove_images=remove_images))
+    return modify_links(input_text)
 
 def read_included_file(path):
     try:
