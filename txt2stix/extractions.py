326c47571bbb3bf26e5fbfe8ec3f08d2c671d597
Fadl <chaos@efqr.dev>
2024-11-22 14:49:56 +0100

    add test cases to ai extractors #86

--- txt2stix/extractions.py
+++ txt2stix/extractions.py
@@ -21,10 +21,13 @@ class Extractor(NamedDict):
     prompt_extraction_extra = None
 
 
-    def __init__(self, key, dct, include_path=None):
+    def __init__(self, key, dct, include_path=None, test_cases: dict[str, list[str]]=None):
         super().__init__(dct)
         self.extraction_key = key
         self.slug = key
+        if test_cases:
+            self.prompt_negative_examples = test_cases.get('test_negative_examples', [])
+            self.prompt_positive_examples = test_cases.get('test_positive_examples', [])
         if self.file and not Path(self.file).is_absolute() and include_path:
             self.file = Path(include_path) / self.file
 
@@ -38,7 +41,14 @@ class Extractor(NamedDict):
 
 def parse_extraction_config(include_path: Path):
     config = {}
+    test_cases = load_test_cases_config(include_path)
     for p in include_path.glob("extractions/*/config.yaml"):
         config.update(yaml.safe_load(p.open()))
     
-    return {k: Extractor(k, v, include_path) for k, v in config.items()}
+    return {k: Extractor(k, v, include_path, test_cases=test_cases.get(v.get('test_cases'))) for k, v in config.items()}
+
+def load_test_cases_config(include_path: Path) -> dict[str, dict[str, list[str]]]:
+    config_file = include_path/'test_cases.yaml'
+    if not config_file.exists():
+        return {}
+    return yaml.safe_load(config_file.open())
