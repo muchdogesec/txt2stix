af3a752c7d40680f742ca4a10bdd2dd22fb196cc
Fadl <chaos@efqr.dev>
2025-11-13 15:44:56 +0100

    change-remote-extension-url

--- txt2stix/bundler.py
+++ txt2stix/bundler.py
@@ -133,7 +133,7 @@ class txt2stixBundler:
         "phone-number": None,
         "weakness": None,
     }
-    EXTENSION_DEFINITION_BASE_URL = "https://raw.githubusercontent.com/muchdogesec/stix2extensions/main/extension-definitions"
+    EXTENSION_DEFINITION_BASE_URL = "https://raw.githubusercontent.com/muchdogesec/stix2extensions/main/automodel_generated/extension-definitions"
     ATTACK_FLOW_SMO_URL = "https://github.com/muchdogesec/stix2extensions/raw/refs/heads/main/remote-definitions/attack-flow.json"
     report = None
     identity = None
