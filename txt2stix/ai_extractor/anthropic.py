3733096f7292212123442fdea13af5f825b02d06
Chaos Pjeles <fqrious@gmail.com>
2025-11-24 12:52:22 +0100

    bump llama-index (#260)
    * bump llama-index
    
    * bump

--- txt2stix/ai_extractor/anthropic.py
+++ txt2stix/ai_extractor/anthropic.py
@@ -6,7 +6,7 @@ from llama_index.llms.anthropic import Anthropic
 
 
 class AnthropicAIExtractor(BaseAIExtractor, provider="anthropic"):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, model='claude-sonnet-4-0', **kwargs) -> None:
         kwargs.setdefault('temperature', float(os.environ.get('TEMPERATURE', 0.0)))
-        self.llm = Anthropic(max_tokens=4096, system_prompt=self.system_prompt, **kwargs)
+        self.llm = Anthropic(max_tokens=4096, model=model, system_prompt=self.system_prompt, **kwargs)
         super().__init__()
